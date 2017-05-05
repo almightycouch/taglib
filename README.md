59d93895150d46cb6ddf366730db99c4440de0e6
Mario Flach <m.flach@almightycouch.org>
2017-05-05 12:21:28 +0200

    Prepare project for Hex

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
 # Taglib
 
-**TODO: Add description**
+Taglib bindings for Elixir.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `taglib` to your list of dependencies in `mix.exs`:
+Add `:taglib` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:taglib, "~> 0.1.0"}]
+  [{:taglib, "~> 0.1"}]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/taglib](https://hexdocs.pm/taglib).
-
--- mix.exs
+++ mix.exs
@@ -1,18 +1,42 @@
 defmodule Taglib.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :taglib,
-     version: "0.1.0",
+     name: "Taglib",
+     version: @version,
      elixir: "~> 1.4",
+     package: package(),
+     description: description(),
      compilers: [:elixir_make] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: docs(),
      deps: deps()]
   end
 
   def application do
     [extra_applications: [:logger]]
+  end
+
+  defp package do
+    [files: ["c_str", "lib", "mix.exs", "Makefile", "README.md", "LICENSE"],
+      maintainers: ["Mario Flach"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/almightycouch/taglib"}]
+  end
+
+  defp description do
+    "Taglib bindings for Elixir"
+  end
+
+  defp docs do
+    [extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/almightycouch/taglib"]
   end
 
   defp deps do
