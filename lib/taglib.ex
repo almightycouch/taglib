bc37dafb1e83a5f90ab2693b04c50b89729b9bd9
Mario Flach <m.flach@almightycouch.org>
2017-05-05 12:30:14 +0200

    Update Taglib docs

--- lib/taglib.ex
+++ lib/taglib.ex
@@ -1,6 +1,15 @@
 defmodule Taglib do
   @moduledoc """
-  Elixir NIF bindings for Taglib.
+  Elixir bindings for Taglib.
+
+  This module provides a simple wrapper around the Taglib C++ API.
+
+      iex> {:ok, ref} = Taglib.new("song.mp3")
+      {:ok, ""}
+      iex> Taglib.tag_title(ref)
+      "Mi Mujer"
+      iex> Taglib.audio_length(ref)
+      438
   """
 
   @on_load :load_nif
@@ -9,6 +18,7 @@ defmodule Taglib do
 
   require Logger
 
+  @doc false
   def load_nif do
     case :erlang.load_nif(@nif_path, 0) do
       :ok -> :ok
@@ -16,12 +26,43 @@ defmodule Taglib do
     end
   end
 
-  def new(_filepath),         do: raise Code.LoadError, file: @nif_path
-  def tag_title(_fileref),    do: raise Code.LoadError, file: @nif_path
-  def tag_artist(_fileref),   do: raise Code.LoadError, file: @nif_path
-  def tag_album(_fileref),    do: raise Code.LoadError, file: @nif_path
-  def tag_genre(_fileref),    do: raise Code.LoadError, file: @nif_path
-  def tag_year(_fileref),     do: raise Code.LoadError, file: @nif_path
-  def tag_track(_fileref),    do: raise Code.LoadError, file: @nif_path
+  @doc """
+  Create a new file reference for the given `filepath`.
+  """
+  def new(_filepath), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the title for the given `fileref`.
+  """
+  def tag_title(_fileref), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the artist for the given `fileref`.
+  """
+  def tag_artist(_fileref), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the album for the given `fileref`.
+  """
+  def tag_album(_fileref), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the genre for the given `fileref`.
+  """
+  def tag_genre(_fileref), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the year for the given `fileref`.
+  """
+  def tag_year(_fileref), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the track number for the given `fileref`.
+  """
+  def tag_track(_fileref), do: raise Code.LoadError, file: @nif_path
+
+  @doc """
+  Returns the audio length for the given `fileref`.
+  """
   def audio_length(_fileref), do: raise Code.LoadError, file: @nif_path
 end
